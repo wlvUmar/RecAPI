d161ab2c4264e8f8e9586e1c588b5da88f657f60
azimovic1 <U.Tolibov@wlv.ac.uk>
2025-05-28 03:30:03 +0500

    changed to postgresql

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Movie Recommendation API"
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
-    DATABASE_URL: str = "sqlite+aiosqlite:///./movies.db"
+    DATABASE_URL: str = "postgres+asyncpg://postgres:zSupTsJT4miboAy@crimson-sea-6932.flycast:5432"
 
 settings = Settings() 
