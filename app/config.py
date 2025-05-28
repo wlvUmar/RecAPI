a2f9719953b064be3246d5203f3a02e7d76d32ea
azimovic1 <U.Tolibov@wlv.ac.uk>
2025-05-29 00:24:37 +0500

    adfsadfadf

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Movie Recommendation API"
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:zSupTsJT4miboAy@crimson-sea-6932.flycast:5432/postgres"
+    DATABASE_URL: str = "postgresql+asyncpg://recapi:BBlWoWaAqtqx0zc@divine-frost-269.flycast:5432/recapi?sslmode=disable"
 # crimson-sea-6932.flycast
 settings = Settings() 
