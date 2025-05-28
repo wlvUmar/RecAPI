e0d4f70b95bebd690c49ea1c163498bd100b5cbe
azimovic1 <U.Tolibov@wlv.ac.uk>
2025-05-29 02:02:10 +0500

    db url updated

--- app/config.py
+++ app/config.py
@@ -1,9 +1,12 @@
 from pydantic_settings import BaseSettings
+import os
+
+db_url = os.getenv("DATABASE_URL")
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Movie Recommendation API"
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
-    DATABASE_URL: str = "postgresql+asyncpg://recapi:BBlWoWaAqtqx0zc@divine-frost-269.flycast:5432/recapi?sslmode=disable"
+    DATABASE_URL: str = "postgresql+asyncpg://recapi:SuLKZxe4m2Tm7TI@recapi-db.flycast:5432/recapi?sslmode=disable"
 
 settings = Settings() 
